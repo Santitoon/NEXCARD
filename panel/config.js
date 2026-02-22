d119d83d1c0766caefafd7937ee866abbe53060c
M4G4 <124450800+Santitoon@users.noreply.github.com>
2026-02-22 17:44:03 -0500

    Update config.js

--- panel/config.js
+++ panel/config.js
@@ -1,7 +1,8 @@
 // config.js
-window.NEXCARD = {
+window.NEXCARD_CONFIG = {
   API_BASE: "https://script.google.com/macros/s/AKfycbx7ghGSC47jyKY5ArI2s1JpO3UILEHVzmfaHxYxG3sLYfN3QDyFYWnXFStRLHvdJGtXRQ/exec",
   TOKEN_KEY: "nexcard_token",
   EMPRESA_KEY: "nexcard_empresa_id",
-  ALIAS_KEY: "nexcard_empresa_alias"
+  ALIAS_KEY: "nexcard_empresa_alias",
+  PAGE_SIZE: 20
 };
